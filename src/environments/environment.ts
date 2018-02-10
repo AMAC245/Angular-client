7352ae33d17621ad6793922be6253b82f23fd23f
Andrew McGillivray <ap.mcgillivray@gmail.com>
2018-02-10 17:14:52 +1100

    add load state animations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,5 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
   production: false,
   BASE_URL: 'https://api.dribbble.com/v1/shots',
   TOKEN: '?access_token=859f432cad2e206603ed29d9c5728f0c14673e2ea14c453b07f3b05a36cd2fea'
 };
-
-
-// export const environment = {
-//     production: false,
-//     BASE_URL: 'http://www.songsterr.com/a/ra/songs.xml?pattern=Marley',
-//     TOKEN: '?access_token=859f432cad2e206603ed29d9c5728f0c14673e2ea14c453b07f3b05a36cd2fea'
-//   };
